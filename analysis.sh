d6b90818c85a533b00d0c740c9e8b7d662c65360
paul-baxter <pebaxter@gmail.com>
2020-09-04 12:07:32 +0100

    main run script fixes

--- analysis.sh
+++ analysis.sh
@@ -3,11 +3,18 @@ clear
 echo "Story Construction Log File Analysis"
 echo ""
 
+cp src/analysis.py analysis.py
+cp src/statsfunctions.py statsfunctions.py
+
 # look for contents of /data...
 for entry in data/*.dat
 do
     #echo "$entry"
-    python3 /src/analysis.py "$entry"
+    python3 analysis.py "$entry"
 done
+
+rm analysis.py
+rm statsfunctions.py
+
 echo "All Done"
 echo ""
